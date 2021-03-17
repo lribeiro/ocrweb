15dc32531b209a596c588b7f60e47658ad170df2
Luis Landeiro Ribeiro <luis.ribeiro@pdmfc.com>
2021-03-17 21:57:33 +0000

    more resilient numpy to int and float conversion

--- app.py
+++ app.py
@@ -9,16 +9,51 @@ app = Flask(__name__)
 
 reader = easyocr.Reader(['pt','en'])
 
+def gr(obj):
+    val = obj
+    if isinstance(obj, numpy.generic):
+        val = obj.item()
+
+    return round(val)
+
+def g(obj):
+    val = obj
+    if isinstance(obj, numpy.generic):
+        val = obj.item()
+
+    return val
+
+@app.route('/ocr_img', methods=["POST"])
+def ocr_img():
+    try:
+        img = request.data
+        output = reader.readtext(img)
+
+        values = []
+        for entry in output:
+            pval = [[ [gr(x[0]),gr(x[1])] for x in entry[0]], entry[1], round(g(entry[2]) * 100,2)]
+            values.append(pval)
+
+        return jsonify({"output": values})
+    except:
+        traceback.print_exc()
+        e = sys.exc_info()[0]
+        print( "<p>Error: %s</p>" % e )
+
+        return jsonify(
+            {"error": "Did you mean to send: {'image_url': 'some_jpeg_url'}"}
+        )
+
 @app.route('/ocr', methods=["POST"])
 def ocr():
     try:
         url = request.json['image_url']
         if url.endswith("jpg") or url.endswith("jpeg") or url.endswith("png"):
             output = reader.readtext(url)
-
+    
             values = []
             for entry in output:
-                pval = [[ [round(x[0].item(())),round(x[1].item())] for x in entry[0]], entry[1], round(entry[2].item() * 100,2)]
+                pval = [[ [gr(x[0]),gr(x[1])] for x in entry[0]], entry[1], round(g(entry[2]) * 100,2)]
                 values.append(pval)
 
             return jsonify({"output": values})
